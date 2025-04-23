40967a9062c65e78c1a52dbe161ed60175e2fbc8
Sean Ostermann <falkyre@gmail.com>
2025-04-22 19:53:31 -0500

    Update main.py
    Update version to 2025.3.5

--- src/main.py
+++ src/main.py
@@ -29,7 +29,7 @@ install(show_locals=True)
 
 SCRIPT_NAME = "NHL-LED-SCOREBOARD"
 
-SCRIPT_VERSION = "2025.3.4"
+SCRIPT_VERSION = "2025.3.5"
 
 # Conditionally load the appropriate driver classes and set the global driver mode based on command line flags
 
