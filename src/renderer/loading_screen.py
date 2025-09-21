6437c064b782a6828f9b6c16610790ee1aa0960b
User Name <first@example.com>
2025-09-20 21:40:51 -0500

    Update to newest rgbmatrix library to be able to use --low-row-addr-type=5 if needed.  Added new 128x64 loading png

--- src/renderer/loading_screen.py
+++ src/renderer/loading_screen.py
@@ -1,12 +1,24 @@
+import os
 from PIL import Image
 from images.image_helper import ImageHelper
 
-PATH = "assets/loading/loading.png"
+PATH = "assets/loading/"
 
 class Loading:
     def __init__(self, matrix):
         self.matrix = matrix
-        self.image = Image.open(PATH)
+
+	# Check to see if loading image for screen size exists, if not, fall back to 
+        # original 64x32 one
+      
+        img = f"{PATH}/loading-{self.matrix.width}x{self.matrix.height}.png"
+        img_ori = f"{PATH}/loading.png"
+
+        if os.path.exists(img):
+           self.image = Image.open(img)
+        else:
+           self.image = Image.open(img_ori)
+
     def render(self):
 
         self.matrix.draw_image(
